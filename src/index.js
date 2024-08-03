195c5299a4b9568a32d3c0cda9bc77e22272558b
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2024-08-03 12:27:23 +0530

    Created Reacted App

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>Created React App, Welcome S</h1>
+    </div>
+  </React.StrictMode>
+);
+
+
