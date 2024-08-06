4118d705b1f989be7480c901ec2be1e7da8a4479
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2024-08-06 23:20:05 +0530

    updatedPlay.jsx

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -4,16 +4,16 @@ import itachi from '../music/senya.mp3'
 function Play(){
 
     return(
-        <div className="container anime-play">
-            <div className="row">
-                <div className="col">
-                    <img src={naruto} className='play-image'   />
+        <div className="anime-play border border-dark" >
+            <div className="row anime-play-top p-3">
+                <div className="col border border-dark">
+                    <img src={naruto} className='play-image' />
                 </div>
-                <div className="col">
+                <div className="col border border-dark">
                     <h1 className='text-primary play-descp'>Itachi Theme</h1>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row align-items-end p-1'>
                 <audio src={itachi} controls></audio>
             </div>
         </div>
